9c35119fdba663d9570a8125f0dc7967cf6a4bac
ChaoNEil <neilborgan@gmail.com>
2023-07-20 12:43:36 +0530

    Refactor / add code

--- script.js
+++ script.js
@@ -1,45 +1,81 @@
+//declare empty array for library
 let myLibrary = [];
 
+
+//object constructor 
 function Book(title,author,pages,read) {
-  // the constructor...
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 
-function render(){
-  let libraryElement = document.getElementById("library");
-  libraryElement.innerHTML = "";
-  for (let i = 0; i < myLibrary.length; i++) {
-    let book = myLibrary[i];
-    let bookElement = document.createElement("div");
-    bookElement.innerHTML = `<p>${book.title}</p>`
-    libraryElement.appendChild(bookElement);
+//for adding a new book to the array
+function addBookToLibrary(title,author,pages,read) {
+  let book = new Book(title,author,pages,read);
+  myLibrary.push(book);
+  displayBooksOnPage();
+}
+
+//function to display library array to cards
+function displayBooksOnPage(){
+  const books = document.querySelector(".books");
+  
+  const removeDivs = document.querySelectorAll(".card");
+  console.log("show me the node cound of current divs..", removeDivs);
+  for (let i = 0; i<removeDivs.length; i++){
+    removeDivs[i].remove();
   }
+
+  //loop over the library array and display to the cards
+  myLibrary.forEach(myLibrary => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    books.appendChild(card);
+    for (let key in myLibrary){
+      console.log(`${key}: ${myLibrary[key]}`);
+      const para = document.createElement("p");
+      para.textContent = (`${key}: ${myLibrary[key]}`);
+      card.appendChild(para);
+    }
+  })
 }
 
-function addBookToLibrary() {
-  // do stuff here
+//event listener for form to add new book to library
+const addBookBtn = document.querySelector(".add-book-btn")
+addBookBtn.addEventListener("click" , function (){
+  document.getElementById("add-book-form").style.display = "";
+})
+
+//event listener input to add new data entry to form
+const submitBtn = document.querySelector(".submit-btn");
+submitBtn.addEventListener("click", intakeFormData);
+
+//transform form data to variables for intake
+function intakeFormData(){
   let title = document.getElementById("title").value;
   let author = document.getElementById("author").value;
   let pages = document.getElementById("pages").value;
-  let read = document.getElementById("read").checked;
-  let newBook = new Book(title,author,pages,read)
-  myLibrary.push(newBook);
-  render();
+  let read = document.getElementById("read").value;
+
+  //break out form if data is incomplete or not valid
+  if ((title === "") || (author === "") || (pages === "") || (read === "")) {
+    return;
+  }
+
+  //call function to input the book data to array
+  addBookToLibrary(title,author,pages,read);
+  
+  //reset the form after successful submission
+  document.getElementById("add-book").reset();
+
 }
 
+//start event listener for clear form button
+const clearButton = document.querySelector(".reset-btn");
+clearButton.addEventListener("click", clearForm);
 
-let newBookbtn = document.getElementById("add-book-btn");
-newBookbtn.addEventListener("click", function() {
-  let newBookForm = document.getElementById("new-book-form");
-  console.log(newBookForm);
-  newBookForm.style.display = "block";
-})
-
-document.getElementById("new-book-form").addEventListener("submit", function(event){
-  event.preventDefault();
-  addBookToLibrary();
-})
+function clearForm(){
+  document.getElementById("add-book").reset();
+}
